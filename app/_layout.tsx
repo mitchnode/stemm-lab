91af7c61629e07697667454e3a82fcee1dbb2b2e
Mitchell Collings <macollings@gmail.com>
2026-03-24 22:21:40 +1100

    Add responsive meta tag to ensure proper rendering and touch zooming with MaterialUI and mobiles, and CssBasline to add a global stylesheet resetto ensure compatbility across devices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,18 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
+import { CssBaseline } from "@mui/material";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <head>
+        <title>STEMM Lab Games</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
+      <CssBaseline />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
