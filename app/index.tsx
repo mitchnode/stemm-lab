562452c54886903d4b611e0074b644f6e9d536eb
Mitchell Collings <macollings@gmail.com>
2026-04-01 23:56:51 +1100

    Create team screen thast stores teamData locally. Add loadTeam to index to check if the team has already been created. Add route to /team if team has not been created yet.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,37 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
+interface Team {
+  id: number;
+  team_name: string;
+  members: string[];
+}
+
 export default function Index() {
+  const router = useRouter();
+  let [team, setTeam] = useState({});
+
+  const loadTeam = async () => {
+    try {
+      const storedTeam = await AsyncStorage.getItem("team");
+      if (storedTeam) {
+        setTeam(JSON.parse(storedTeam));
+        console.log("Team loaded from storage", storedTeam);
+      } else {
+        console.log("No Team created yet");
+        router.push("/team");
+      }
+    } catch (error) {
+      console.error("Error loading team:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadTeam();
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +40,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome {team.team_name}</Text>
     </View>
   );
 }
---
+++ app/team.tsx
@@ -0,0 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Alert, Button, Text, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+  let [team, setTeam] = useState({});
+
+  const storeTeam = async () => {
+    try {
+      const teamData = {
+        id: 1,
+        team_name: "Test Team",
+        members: ["Adam", "Brad", "Chris"],
+      };
+      await AsyncStorage.setItem("team", JSON.stringify(teamData));
+      setTeam(teamData);
+      Alert.alert("Success", "Team saved locally");
+      router.push("/");
+    } catch (error) {
+      console.error("Error saving team:", error);
+    }
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>STEMM Lab Games</Text>
+
+      <Button title="Add Member" />
+      <Button title="Create Team" onPress={storeTeam} />
+    </View>
+  );
+}
