d8ccba40806f637e92664999206c30217aa8f92f
Mitchell Collings <macollings@gmail.com>
2026-04-01 21:20:02 +1100

    Remove MUI library due to issue with Expo Go

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,8 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { CssBaseline } from "@mui/material";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <>
-      <head>
-        <title>STEMM Lab Games</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
-      <CssBaseline />
       <Stack screenOptions={{ headerShown: false }} />
     </>
   );
