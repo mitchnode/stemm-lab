46fef38867f61350d2a74bead08189a7026c3651
Mitchell Collings <macollings@gmail.com>
2026-04-03 21:53:15 +1100

    Add Theme provider to entry point

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,13 @@
+import { ThemeProvider } from "@/theme";
 import { Stack } from "expo-router";
-/* <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="team" options={{ presentation: "modal" }} />
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider> */
+
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="team" options={{ presentation: "modal" }} />
-    </Stack>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="team" options={{ presentation: "modal" }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
