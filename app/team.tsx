d516939e3ab5b27a6e4d1ddf1939f8f07089ebde
Mitchell Collings <macollings@gmail.com>
2026-04-04 23:19:33 +1100

    Change form fields to ControlledInput and ControlledSelect. Add validation for members and change added members to text list with individual remove buttons.

--- app/team.tsx
+++ app/team.tsx
@@ -5,15 +5,29 @@ import { useRouter } from "expo-router";
 import {
   Box,
   Button,
+  ControlledInput,
+  ControlledSelect,
   Input,
-  Select,
   Text,
   useTheme as useRETheme,
 } from "re-native-ui";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Alert, Pressable, StyleSheet, View } from "react-native";
 
+interface Member {
+  id: number;
+  name: string;
+}
+interface Team {
+  id: number;
+  team_name: string;
+  year: string;
+  members: string[];
+}
+
 export default function Index() {
+  const router = useRouter();
   const { colors } = useTheme();
   // Override theme from re-native-ui
   const theme = useRETheme();
@@ -22,88 +36,7 @@ export default function Index() {
   theme.colors.text = colors.text;
   theme.colors.border = colors.border;
 
-  const router = useRouter();
-  const [team, setTeam] = useState({
-    id: 0,
-    team_name: "",
-    year: "",
-    members: [""],
-  });
-  const [member_inputs, setMemberInputs] = useState([
-    { id: Date.now(), value: "", disabled: false },
-  ]);
   const MAX_MEMBERS = 5;
-
-  const storeTeam = async (teamData: {
-    id: number;
-    team_name: string;
-    year: string;
-    members: string[];
-  }) => {
-    try {
-      await AsyncStorage.setItem("team", JSON.stringify(teamData));
-      Alert.alert("Success", "Team saved locally");
-      router.push("/");
-    } catch (error) {
-      console.error("Error saving team:", error);
-    }
-  };
-
-  const createTeam = () => {
-    member_inputs.map((item) => {
-      if (item.value !== "") {
-        members.push(item.value);
-      }
-    });
-    let id = 0;
-    if (team.id == 0) {
-      id = Date.now();
-    } else {
-      id = team.id;
-    }
-
-    const teamData = {
-      id: id,
-      team_name: team_name,
-      year: year,
-      members: members,
-    };
-    setTeam(teamData);
-    storeTeam(teamData);
-  };
-
-  const updateTeamMemberInput = (value: string) => {
-    member_inputs[member_inputs.length - 1].value = value;
-  };
-
-  const addTeamMember = () => {
-    const value = member_inputs[member_inputs.length - 1].value;
-    if (value != "") {
-      member_inputs.map((item) => (item.disabled = true));
-      if (member_inputs.length < MAX_MEMBERS) {
-        setMemberInputs([
-          ...member_inputs,
-          { id: Date.now(), value: "", disabled: false },
-        ]);
-      } else {
-        Alert.alert("Team Full", `Maximum team members is ${MAX_MEMBERS}`);
-      }
-    } else {
-      Alert.alert("Member Name Required", "Please enter a member name");
-    }
-  };
-
-  const removeTeamMember = () => {
-    if (member_inputs.length > 1) {
-      member_inputs.pop();
-      member_inputs[member_inputs.length - 1].disabled = false;
-      setMemberInputs([...member_inputs]);
-    }
-  };
-
-  const [team_name, onChangeTeamName] = useState("");
-  const [year, setYear] = useState("");
-  const [members, setMembers] = useState<string[]>([]);
 
   const yearData = [
     { label: "Year 4", value: "4" },
@@ -114,8 +47,82 @@ export default function Index() {
     { label: "Year 9", value: "9" },
   ];
 
-  /*----------------Look at controlledInput and controlledSelect in
-            form management in re-native-ui----------------*/
+  const [team, setTeam] = useState<Team | null>(null);
+  const [members_text, setMembers] = useState<Member[]>([]);
+  const [member_input, setMemberInput] = useState("");
+  const [isMemberErrorVisible, setMemberError] = useState(false);
+  const [isMemberMaxVisible, setMemberMax] = useState(false);
+  const [isMemberEmptyVisible, setMemberEmpty] = useState(false);
+  const { control, handleSubmit } = useForm({
+    defaultValues: { team_name: "", year: "", members: [] },
+  });
+
+  const storeTeam = async (teamData: Team) => {
+    try {
+      await AsyncStorage.setItem("team", JSON.stringify(teamData));
+      Alert.alert("Success", "Team saved locally");
+      router.push("/");
+    } catch (error) {
+      console.error("Error saving team:", error);
+    }
+  };
+
+  const memberInput = (value: string) => {
+    setMemberEmpty(false);
+    setMemberInput(value);
+  };
+  const createTeam = (data: any) => {
+    setMemberError(false);
+    console.log(members_text.length);
+    if (members_text.length > 0) {
+      let id = 0;
+      if (team == null) {
+        id = Date.now();
+      } else {
+        id = team.id;
+      }
+
+      const members_array: string[] = [];
+      members_text.map((items) => {
+        members_array.push(items.name);
+      });
+
+      const teamData = {
+        id: id,
+        team_name: data.team_name,
+        year: data.year,
+        members: members_array,
+      };
+      console.log(teamData);
+      setTeam(teamData);
+      storeTeam(teamData);
+    } else {
+      console.log("Need members");
+      setMemberError(true);
+    }
+  };
+
+  const addTeamMember = () => {
+    if (member_input != "") {
+      setMemberEmpty(false);
+      setMemberError(false);
+      if (members_text.length < MAX_MEMBERS) {
+        setMembers([...members_text, { id: Date.now(), name: member_input }]);
+        setMemberInput("");
+      } else {
+        setMemberMax(true);
+      }
+    } else {
+      setMemberEmpty(true);
+    }
+  };
+
+  const removeTeamMember = (item: Member) => {
+    setMemberMax(false);
+    const updatedMembers = members_text.filter((member) => member !== item);
+    setMembers(updatedMembers);
+  };
+
   return (
     <View
       style={{
@@ -128,34 +135,55 @@ export default function Index() {
       </Text>
       <View style={{ ...styles.container, backgroundColor: colors.background }}>
         <Box p="md" style={{ ...styles.box, backgroundColor: colors.surface }}>
-          <Input
+          <ControlledInput
+            name="team_name"
+            label="Team Name"
+            control={control}
+            rules={{ required: "Team Name is required" }}
             style={{ ...styles.input, backgroundColor: colors.background }}
-            onChangeText={onChangeTeamName}
-            value={team_name}
             placeholder="Enter Team Name"
           />
-          <Select
-            placeholder="Select Year"
-            value={year}
-            onChange={setYear}
+          <ControlledSelect
+            name="year"
+            label="Year"
             options={yearData}
+            control={control}
+            rules={{ required: "Year is required" }}
+            placeholder="Select Year"
           />
-          <View style={styles.row}>
-            <View style={styles.member_input}>
-              {member_inputs.map((item) => (
-                <Input
-                  key={item.id}
-                  style={{
-                    ...styles.input,
-                    backgroundColor: colors.background,
-                  }}
-                  placeholder="Enter Member Name"
-                  editable={!item.disabled}
-                  onChangeText={(newText) => updateTeamMemberInput(newText)}
-                />
-              ))}
-            </View>
-            <View style={styles.member_buttons}>
+          <Text style={styles.label}>Member Names</Text>
+
+          <View style={styles.member_input}>
+            <View style={styles.row}>
+              <View style={styles.input_container}>
+                {isMemberEmptyVisible ? (
+                  <Input
+                    style={{
+                      ...styles.input,
+                      borderColor: colors.error,
+                      backgroundColor: colors.background,
+                    }}
+                    placeholder="Enter Member Name"
+                    onChangeText={memberInput}
+                    value={member_input}
+                  />
+                ) : (
+                  <Input
+                    style={{
+                      ...styles.input,
+                      backgroundColor: colors.background,
+                    }}
+                    placeholder="Enter Member Name"
+                    onChangeText={memberInput}
+                    value={member_input}
+                  />
+                )}
+                {isMemberEmptyVisible && (
+                  <Text style={{ ...styles.member_error, color: colors.error }}>
+                    Member name cannot be blank
+                  </Text>
+                )}
+              </View>
               <View style={styles.member_button_view}>
                 <Pressable
                   style={{
@@ -167,14 +195,18 @@ export default function Index() {
                   <Ionicons name="add-outline" size={32} color={colors.text} />
                 </Pressable>
               </View>
-              {member_inputs.length > 1 ? (
-                <View style={styles.member_button_view}>
+            </View>
+            <View style={styles.members}>
+              {members_text.map((item) => (
+                <View key={item.id} style={styles.row}>
+                  <Text style={styles.member_text}>{item.name}</Text>
                   <Pressable
+                    id={item.id.toString()}
                     style={{
                       ...styles.member_button,
                       backgroundColor: colors.secondary,
                     }}
-                    onPress={removeTeamMember}
+                    onPress={() => removeTeamMember(item)}
                   >
                     <Ionicons
                       name="remove-outline"
@@ -183,11 +215,21 @@ export default function Index() {
                     />
                   </Pressable>
                 </View>
-              ) : null}
+              ))}
+              {isMemberErrorVisible && (
+                <Text style={{ ...styles.member_error, color: colors.error }}>
+                  Need at least 1 team member
+                </Text>
+              )}
+              {isMemberMaxVisible && (
+                <Text style={{ ...styles.member_error, color: colors.error }}>
+                  {`Maximum team members is ${MAX_MEMBERS}`}
+                </Text>
+              )}
             </View>
           </View>
         </Box>
-        <Button onPress={createTeam}>Create Team</Button>
+        <Button onPress={handleSubmit(createTeam)}>Create Team</Button>
       </View>
     </View>
   );
@@ -214,16 +256,23 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: "row",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "stretch",
     gap: 20,
+  },
+  input_container: {
+    gap: 2,
   },
   title: {
     textAlign: "center",
     marginTop: 100,
   },
+  label: {
+    paddingBottom: 5,
+  },
   input: {
     flex: 1,
+    marginBottom: 0,
   },
   member_input: {
     flex: 1,
@@ -236,12 +285,22 @@ const styles = StyleSheet.create({
   member_button_view: {
     marginBottom: 16,
   },
-
   member_button: {
     width: 42,
     height: 42,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 30,
+  },
+  members: {
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    gap: 10,
+  },
+  member_text: {
+    verticalAlign: "middle",
+  },
+  member_error: {
+    fontSize: 14,
   },
 });
